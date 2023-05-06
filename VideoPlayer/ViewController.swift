3d7ff2d9c38af5f4f3d4506489695c3a5e2bfabb
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2023-05-06 11:57:50 -0600

    Interfaz terminada

--- VideoPlayer/ViewController.swift
+++ VideoPlayer/ViewController.swift
@@ -8,11 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var videosCollection: UICollectionView!
+    @IBOutlet weak var categoriasVideos: UISegmentedControl!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func buscarButton(_ sender: UIBarButtonItem) {
+    }
+    
+    
+    @IBAction func categoriaSeleccionada(_ sender: UISegmentedControl) {
+    }
+    
 
 
 }
