f5fc138958530f93f831ff2f04f610df4b45292d
Marco Alonso Rodriguez Tapia <mrodriguez@alquimiapay.com>
2023-05-06 11:42:29 -0600

    Iniciando

---
+++ VideoPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoPlayer
+//
+//  Created by Marco Alonso Rodriguez on 06/05/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
